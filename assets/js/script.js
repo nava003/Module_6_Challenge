3ab4aa30f14e1b9b2d6d50359c19d6fe196013fb
NavaHub <navaa003@gmail.com>
2023-08-07 16:08:14 -0500

    Renamed search history card div in html; Renamed a function to better describe it's purpose, changed one parameter in a URL to acquire the current weather vs the forecast weather for now, and added new eventlistener to searchBtn to create logic on saving city name inputs on the website.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,41 @@
 var searchBtn = document.getElementById('searchBtn');
 var searchCity = document.getElementById('cityName');
+var searchHistoryCard = document.getElementById('searchHistoryCard');
+var cityWeatherCard = document.getElementById('cityWeatherCard');
 
 var openWeatherAPI = "5a53411e129f903ae31c1c2a4945c078";
 // var stringSample = "London";
+var citySearchList = [];
+
+function setHistory() {
+    citySearchList.push(searchCity.value);
+
+    for (var i = 0; i < citySearchList.length; i++) {
+        var btnEl = $('button');
+        btnEl.attr('button', (i + 1));
+    }
+}
 
 function getGeoCoords() {
     var openWeatherGeoURL = "https://api.openweathermap.org/geo/1.0/";
 
     fetch(`${openWeatherGeoURL}direct?q=${searchCity.value}&appid=${openWeatherAPI}`)
         .then(function (response) {
-            console.log(response);
+            // console.log(response);
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
+            // console.log(data);
+            var dataLat = data[0].lat;
+            var dataLong = data[0].lon;
+            getCurrentWeather(dataLat, dataLong);
         });
 }
 
-function getWeatherAPI(lat, lon) {
+function getCurrentWeather(lat, lon) {
     var openWeatherDayURL = "https://api.openweathermap.org/data/2.5/";
 
-    fetch(`${openWeatherDayURL}forecast?lat=${lat}&lon=${lon}&appid=${openWeatherAPI}`)
+    fetch(`${openWeatherDayURL}weather?lat=${lat}&lon=${lon}&appid=${openWeatherAPI}&units=imperial`)
         .then(function (response) {
             console.log(response);
             return response.json();
@@ -30,4 +45,5 @@ function getWeatherAPI(lat, lon) {
         });
 }
 
+searchBtn.addEventListener('click', setHistory);
 searchBtn.addEventListener('click', getGeoCoords);
